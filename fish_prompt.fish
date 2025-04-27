f551636a6e168e077660d8d59aa65b50397ff84f
Devsstudent <devsstudent777@gmail.com>
2025-04-27 19:39:57 +0200

    reworked the basic left prompt

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -6,31 +6,52 @@ function flash_trd; set_color -o 666; end
 function flash_dim; set_color -o 333; end
 function flash_off; set_color normal; end
 function bc; command bc -l $argv; end
+function blue; set_color -o 3dc1d3; end
+function pink; set_color -o f78fb3; end
+function deep_rose; set_color -o c44569; end
+function porcelain; set_color -o e66767; end
+function reset_color; set_color normal; end
+function hard_blue; set_color -o 546de5; end
 
 function fish_prompt
-  set -l code $status
+  #set -l code $status
 
+ # set -l prompt (prompt_pwd)
+ # set -l base (basename "$prompt")
   set -l prompt (prompt_pwd)
-  set -l base (basename "$prompt")
+  set -l base (basename "$PWD")
 
-  printf (flash_snd)"( "(begin
-    if test "$PWD" = "/"
-      test $code -eq 0; and echo (flash_fst)"/"(flash_off); or echo (flash_dim)"/"(flash_off)
-    else
-      echo ""
-    end
-  end)(echo "$prompt" \
-  | sed "s|~|"(begin
-      test $code -eq 0; and echo (flash_fst); or echo (flash_dim)
-    end)"⌁"(flash_off)"|g" \
-  | sed "s|/|"(flash_snd)" ) "(flash_off)"|g" \
-  | sed "s|"$base"|"(flash_fst)$base(flash_off)" |g")(flash_snd)(begin
-    test "$PWD" = "$HOME"; and echo " "; echo ""
-    end)(begin
-      if test "$PWD" = "/"
-        echo ""
+  set -l pp (string replace -r "^$HOME" " ⌁" $PWD)  # Replace home dir with ~
+  set -l counter 1
+  set -l homeIcon 0
+
+  # Checking what is the first char a / or a ~
+  switch $PWD
+  case "$HOME*"
+  	set -l pp (string replace -r "^$HOME" " ⌁" $PWD)  # Replace home dir with ~
+    set homeIcon 1
+  case "*"
+    printf (flash_snd)"/ "(flash_off)
+  end
+  # Split the path into parts
+  set -l parts (string split "/" (string replace -r '^/' '' $pp))
+  for part in $parts
+    # check si current repo name
+    if test "$part" = "$base"
+      printf (hard_blue)$part(flash_off)" >_ "
+    else if test $counter = 1
+      # logic 
+      if test $homeIcon = 1
+		printf (blue)$part(reset_color)
       else
-        echo ") "
+		printf (flash_snd)$part(reset_color)
       end
-    end)(flash_off)
+      printf " :\ "
+    else
+      printf (deep_rose)$part(flash_off)" \ "
+    end
+	set counter (math $counter + 1)
+  end
+
+  printf (flash_off)
 end
